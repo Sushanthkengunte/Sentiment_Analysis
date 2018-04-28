a852940995f654cf6b418080237ca872c8a97241
Sushanthkengunte <sushanthkengunte@gmail.com>
2018-04-28 13:19:32 -0400

    Finished Processing

--- featureSet1.py
+++ featureSet1.py
@@ -2,6 +2,11 @@ import nltk
 import gatheringInputs as Processing
 import featureWords as fw
 from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt;
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+import numpy as np
+
 
 def document_features(document, word_features):
     document_words = set(document)
@@ -13,8 +18,8 @@ def document_features(document, word_features):
 documents = fw.documents
 featuresets = [(document_features(d,fw.word_features), c) for (d,c) in documents]
 
-amazonDocument = Processing.reviewsArray
-reviewFeatureSets = [(document_features(each,fw.word_features))for each in amazonDocument]
+# amazonDocument = Processing.reviewsArray_copy
+# reviewFeatureSets = [(document_features(each,fw.word_features))for each in amazonDocument]
 
 
 train_set, test_set = featuresets[1000:], featuresets[:1000]
@@ -24,29 +29,74 @@ classifier = nltk.NaiveBayesClassifier.train(train_set)
 print (nltk.classify.accuracy(classifier, test_set))
 # decision tree, SVM,
 
-positive = list()
-negative = list()
+# positive = list()
+# negative = list()
 predictions = list()
+graphDict = {}
 
-for i,each in enumerate(reviewFeatureSets):
-    temp = classifier.classify(each)
-    predictions.append((Processing.reviews[i],temp))
+def main():
+    count = 0
+    Processing.main()
+    for key in Processing.tokenizedArrayDict.keys():
+
+        tokenizedDocuments = Processing.tokenizedArrayDict[key]
+        reviewFeatureSets = [(document_features(each, fw.word_features)) for each in tokenizedDocuments]
+        for i, one in enumerate(reviewFeatureSets):
+            temp = classifier.classify(one)
+            reviewsArray = Processing.arrayDict[key]
+            predictions.append((reviewsArray[i], temp))
+        positive = list()
+        negative = list()
+        for predict in predictions:
+            if (predict[1] == 'pos'):
+                positive.append(predict[0])
+            else:
+                negative.append(predict[0])
+        posName = 'Output/positive_' + Processing.quartKeys[count] + '.txt'
+        negName = 'Output/negative_' + Processing.quartKeys[count] + '.txt'
+        file = open(posName, 'w', encoding="utf-8")
+        print(Processing.quartKeys[count], 'Positive Count',len(positive))
+
+        output = " =>POSITIVE\n".join(positive)
+        # print(output)
+        file.write(output)
+        file.close()
+
+        output = ""
+        file = open(negName, 'w', encoding="utf-8")
+        negativeOutput = [each for each in negative if each]
+        print(Processing.quartKeys[count], 'Negative Count', len(negative))
+        output = " =>NEGATIVE\n".join(negativeOutput)
+        file.write(output)
+        file.close()
+        graphDict[Processing.quartKeys[count]] = (len(positive),len(negative))
+        count+= 1
 
 
-for each in predictions:
-    if(each[1] == 'pos'):
-        positive.append(each[0])
-    else:
-        negative.append(each[0])
+main()
+positiveForGraph = []
+neagativeForGraph = []
+# objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+# y_pos = np.arange(len(objects))
+objects = []
+for eacgQuarterPositive in graphDict.keys():
+    objects.append(eacgQuarterPositive)
+    quart = graphDict[eacgQuarterPositive]
+    positiveForGraph.append(quart[0])
+    neagativeForGraph.append(quart[1])
 
+y_pos = np.arange(len(objects))
 
+plt.bar(y_pos, positiveForGraph, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('positive')
+plt.title('Quarter wise positive')
 
-file = open('Output/positive_SVM.txt', 'w')
-output = " =>POSITIVE\n".join(positive)
-file.write(output)
-file.close()
+plt.show()
 
-file = open('Output/negative_SVM.txt', 'w')
-output = " =>NEGATIVE\n".join(negative)
-file.write(output)
-file.close()
+plt.bar(y_pos, neagativeForGraph, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Negative')
+plt.title('Quarter wise negative')
+
+plt.show()
--- gatheringInputs.py
+++ gatheringInputs.py
@@ -2,20 +2,59 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+import os
+
+
 
 # Reading tweets from file
-InputFile = "Input/file201701_201812.csv"
-sd = pd.read_csv(InputFile)
-data = sd[sd.columns[5]].values
-reviewsArray = np.array(data)
+AmazonQ1 = "Data/output_got_amazon_Qrt1.csv"
+AmazonQ2 = "Data/output_got_amazon_Qrt2.csv"
+AmazonQ3 = "Data/output_got_amazon_Qrt3.csv"
+AmazonQ4 = "Data/output_got_amazon_Qrt4.csv"
 
+WalmartQ1 = "Data/output_got_Walmart_Qrt1.csv"
+WalmartQ2 = "Data/output_got_Walmart_Qrt2.csv"
+WalmartQ3 = "Data/output_got_Walmart_Qrt3.csv"
+WalmartQ4 = "Data/output_got_Walmart_Qrt4.csv"
+
+EbayQ1 = "Data/eBay-1st-Quarter.csv"
+EbayQ2 = "Data/eBay-2nd-Quarter.csv"
+EbayQ3 = "Data/eBay-3rd-Quarter.csv"
+EbayQ4 = "Data/eBay-4th-Quarter.csv"
+
+TargetQ1 = "Data/Target-1st-Quarter.csv"
+TargetQ2 = "Data/Target-2nd-Quarter.csv"
+TargetQ3 = "Data/Target-3rd-Quarter.csv"
+TargetQ4 = "Data/Target-4th-Quarter.csv"
+
+BestBuyQ1 = "Data/output_got_bestbuy_Qrt1.csv"
+BestBuyQ2 = "Data/output_got_bestbuy_Qrt2.csv"
+BestBuyQ3 = "Data/output_got_bestbuy_Qrt3.csv"
+BestBuyQ4 = "Data/output_got_bestbuy_Qrt4.csv"
+
+# quartKeys = {0:'aq1',1:'aq2',2:'aq3',3:'aq4',
+#         4:'wq1',5:'wq2',6:'wq3',7:'wq4'}
+
+# quartKeys = {0:'eb1',1:'eb2',2:'eb3',3:'eb4',
+#         4:'ta1',5:'ta2',6:'ta3',7:'ta4'}
+
+quartKeys = {0:'bb1',1:'bb2',2:'bb3',3:'bb4'}
+
+# files = [AmazonQ1,AmazonQ2,AmazonQ3,AmazonQ4,WalmartQ1,WalmartQ2,WalmartQ3,WalmartQ4]
+files = [BestBuyQ1,BestBuyQ2,BestBuyQ3,BestBuyQ4]
+# files = [EbayQ1,EbayQ2,EbayQ3,EbayQ4,TargetQ1,TargetQ2,TargetQ3,TargetQ4]
+
+
+words = set(nltk.corpus.words.words())
 
 #Preprocessing
 EMOJI_NAME_REGEX = re.compile('<Emoji:.*>')
 HASHTAGS_REGEX = re.compile('#\w*')
 MENTIONS_REGEX = re.compile('@[^\s]+')
-LINK_REGEX = re.compile('https?://[^\s]+')
-Link_SECOND_REGEX = re.compile('(\w*\.)+com/[^\s]+')
+# LINK_REGEX = re.compile('https?://[^\s]+')
+
+LINK_REGEX = re.compile('https?://(.*/[\s]?)?(.*_)?[^\s]+')
+# Link_SECOND_REGEX = re.compile('(\w*\.)+com/[^\s]+')
 EXTRA_SPACES_REGEX = re.compile('\s{2,}')
 
 
@@ -30,7 +69,7 @@ pattern = r''' (?x)
             |\.
 
             '''
-reviewsArray_copy = []
+# reviewsArray_copy = []
 
 def preprocess_hashtags(tweet):
     return HASHTAGS_REGEX.sub('', tweet)    #Remove Hastags
@@ -47,15 +86,15 @@ def remove_extra_spaces(tweet):
 def remove_hyperlinks(tweet):
     return LINK_REGEX.sub('', tweet)        #Remoce Hyperlinks
 
-def remove_secondHyperlinks(tweet):
-    return Link_SECOND_REGEX.sub('', tweet)
+# def remove_secondHyperlinks(tweet):
+#     return Link_SECOND_REGEX.sub('', tweet)
 
 preprocessing_pipeline = [
     preprocess_hashtags,
     preprocess_mentions,
     remove_hyperlinks,
     preprocess_Emojis,
-    remove_secondHyperlinks,
+    # remove_secondHyperlinks,
     remove_extra_spaces
 ]
 
@@ -72,16 +111,56 @@ def alpha_filter(w):
     else:
         return False
 
+def isNan(tweet):
+    return tweet != tweet
+
+arrayDict = {}
+tokenizedArrayDict = {}
+arrayTodelete = []
+def populateDictionaries(reviewsArray,key):
+    reviewsArray_copy = []
+    # reviewsArray = np.array(reviewsArray)
+    for i, raw_tweet in enumerate(reviewsArray):
+        if isNan(raw_tweet):
+            arrayTodelete.append(i)
+            continue
+        reviewsArray[i] = preprocess_tweet(raw_tweet, preprocessing_pipeline)
+    reviewsArray = np.delete(reviewsArray, arrayTodelete)
+    for i, raw_tweet in enumerate(reviewsArray):
+        reviewsArray_copy.append(nltk.regexp_tokenize(reviewsArray[i], pattern))
+        reviewsArray_copy[i] = [eachToken for eachToken in reviewsArray_copy[i] if eachToken.lower() in words and not alpha_filter(eachToken) and eachToken != "via"]
+    # print(len(np.delete(reviewsArray,arrayTodelete)))
+
+    # print(len(reviewsArray),len(reviewsArray_copy))
+    arrayDict[key] = reviewsArray
+    tokenizedArrayDict[key] = reviewsArray_copy
+
+# alpha_filter(eachToken)
 
 
 
 def main():
-    for i, raw_tweet in enumerate(reviewsArray):
-        reviewsArray[i] = preprocess_tweet(raw_tweet, preprocessing_pipeline)
-        reviewsArray_copy.append(nltk.regexp_tokenize(reviewsArray[i], pattern))
-        reviewsArray_copy[i] = [eachToken for eachToken in reviewsArray_copy[i] if not alpha_filter(eachToken)]
+    for i in range(len(files)):
+        sd = pd.read_csv(files[i])
+        data = sd[sd.columns[4]].values
+        reviewsArray = np.array(data)
+        populateDictionaries(reviewsArray,quartKeys[i])
+        # output = ""
+        # name = 'Output/' + quartKeys[i] + '.txt'
+        # file = open(name, 'w', encoding="utf-8")
+        # temp = tokenizedArrayDict[quartKeys[i]]
+        # for each in temp:
+        #     sentence = " ".join(each)
+        #     output += sentence+'\n'
+        # file.write(output)
+        # file.close()
 
-main()
+
+    # print(arrayDict,tokenizedArrayDict)
+# main()
+
+
+
 
 
 
